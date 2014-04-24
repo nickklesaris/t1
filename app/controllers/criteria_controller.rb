c79c4e5a90674c59609b81287e72b391a3d53a26
Nick Klesaris <nickklesaris@gmail.com>
2014-04-23 22:48:29 -0400

    last commit before nesting

--- app/controllers/criteria_controller.rb
+++ app/controllers/criteria_controller.rb
@@ -6,4 +6,19 @@ class CriteriaController < ApplicationController
   def index
     @criteria = Criterion.all
   end
-end
+
+  def create
+    @criterion = Criterion.new(criterion_params)
+      if @criterion.save
+        redirect_to 'criteria/new'
+        flash[:notice] = "Criterion created."
+      else
+        render 'new'
+      end
+  end
+
+  private
+    def criterion_params
+      params.require(:criterion).permit(:name, :description)
+    end
+  end
